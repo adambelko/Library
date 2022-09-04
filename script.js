a085135f95d4daaa6817f1c5b2c10d787de4d305
Adzi <belkoadam@gmail.com>
2022-09-04 11:19:29 +1000

    delete btn added with functionality, visually ok, but addEventListener ingetDeleteBtn fn running multiple times?

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ const nameInput = document.querySelector(".name-input");
 const authorInput = document.querySelector(".author-input");
 const bookStatus = document.querySelector(".book-status");
 const myTable = document.querySelector(".my-table");
+submitBtn.addEventListener("click", e => {
+    addBookToLibrary(e);
+    getDeleteBtn();
+    clearForm();
+});
 
-submitBtn.addEventListener("click", e => addBookToLibrary(e));
-
+let bookIndex = 0;
 
 let myLibrary = [
     {name: "Karma", author: "SadhGuru", status: "read"},
-    {name: "12 rules of life", author: "Jordan Peterson", status: "read"}, 
+    {name: "12 Rules Of Life", author: "Jordan Peterson", status: "read"}, 
+    {name: "Atomic Habits", author: "James Clear", status: "not read"}, 
 ];
 
 function Books(name, author, status) {
@@ -24,27 +29,55 @@ function addBookToLibrary(e) {
     let newBook = new Books(nameInput.value, authorInput.value, bookStatus.value);
     myLibrary.push(newBook);
     console.log(myLibrary);
-    
-    displayBooks(newBook);
+    displayBook(newBook);
 }
 
-function displayBooks(book) {
-    let row = document.createElement('tr');
-
-     Object.values(book).forEach(text => {
-        let cell = document.createElement('td');
+function displayBook(book) {
+    let row = document.createElement("tr");
+    row.setAttribute("book-index", bookIndex++);
+    let cellForBtn = document.createElement("td");
+    let deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("class", "delete-btn");
+    deleteBtn.textContent = "Delete";
+    
+    Object.values(book).forEach(text => {
+        let cell = document.createElement("td");
         let textNode = document.createTextNode(text);
         cell.appendChild(textNode);
         row.appendChild(cell);
     })
+
     myTable.appendChild(row);
+    row.appendChild(cellForBtn);
+    cellForBtn.appendChild(deleteBtn);
 }
 
 function displayAllBooks() {
     myLibrary.forEach(book => {
-        displayBooks(book);
+        displayBook(book);
     });
 }
 
+function deleteBook(e) {
+    e.remove();
+    let index = e.getAttribute("book-index");
+    myLibrary.splice(index, 1);
+    console.log(myLibrary);
+}
+
+function getDeleteBtn() {
+    const allDeleteBtns = document.querySelectorAll(".delete-btn");
+    allDeleteBtns.forEach(btn => {
+        btn.addEventListener("click", (e) => deleteBook(e.target.parentNode.parentNode))
+    });
+}
+
+function clearForm() {
+    nameInput.value = "";
+    authorInput.value = "";
+}
+
 displayAllBooks();
+getDeleteBtn();
+
 
